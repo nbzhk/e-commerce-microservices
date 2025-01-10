2d31e7879b750931a5e8f1dc422cbc62a5ba50b4
Nikolay <rumenovnikolay@gmail.com>
2025-01-11 00:30:41 +0100

    refactor

--- eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerDemoApplication {
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerDemoApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 
 }
