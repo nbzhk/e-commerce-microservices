17b500807d5d4a1fe7c7136546ba461935a66645
Nikolay <rumenovnikolay@gmail.com>
2025-01-11 01:22:53 +0100

    api gateway integration

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,28 @@ package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.gateway.route.RouteLocator;
+
 
 @SpringBootApplication
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("auth-service", r -> r
+                        .path("/auth/**")
+                        .uri("http://localhost:8082"))
+                .route("user-service", r -> r
+                        .path("/api/users/**")
+                        .uri("http://localhost:8081"))
+                .build();
     }
 
 }
