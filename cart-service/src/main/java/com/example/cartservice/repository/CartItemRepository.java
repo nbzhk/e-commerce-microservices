18bc862010b063f7ada42b05c8d51e07a32a3f8d
Nikolay <rumenovnikolay@gmail.com>
2025-01-25 20:16:34 +0100

    added cart-service repositories

---
+++ cart-service/src/main/java/com/example/cartservice/repository/CartItemRepository.java
@@ -0,0 +1,9 @@
+package com.example.cartservice.repository;
+
+import com.example.cartservice.model.entity.CartItemEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItemEntity, Long> {
+}
