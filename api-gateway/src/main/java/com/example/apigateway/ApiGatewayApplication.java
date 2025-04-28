84e8625294e78655cc468d6f09d82ca48d4c1b1e
Nikolay <rumenovnikolay@gmail.com>
2025-04-28 10:37:13 +0200

    changed gateway to work with env variables

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -1,5 +1,6 @@
 package com.example.apigateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
@@ -10,6 +11,18 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 @SpringBootApplication
 public class ApiGatewayApplication {
 
+    @Value("${api.gateway.auth.uri}")
+    private String authServerUrl;
+
+    @Value("${api.gateway.user.uri}")
+    private String userServiceUrl;
+
+    @Value("${api.gateway.product.uri}")
+    private String productServiceUrl;
+
+    @Value("${api.gateway.cart.uri}")
+    private String cartServiceUrl;
+
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
@@ -19,16 +32,16 @@ public class ApiGatewayApplication {
         return builder.routes()
                 .route("auth-service", r -> r
                         .path("/auth/**")
-                        .uri("http://localhost:8082"))
+                        .uri(authServerUrl))
                 .route("user-service", r -> r
                         .path("/api/users/**")
-                        .uri("http://localhost:8081"))
+                        .uri(userServiceUrl))
                 .route("product-service", r -> r
                         .path("/api/products/**")
-                        .uri("http://localhost:8083"))
+                        .uri(productServiceUrl))
                 .route("cart-service", r -> r
                         .path("/api/carts/**")
-                        .uri("http://localhost:8084"))
+                        .uri(cartServiceUrl))
                 .build();
     }
 
